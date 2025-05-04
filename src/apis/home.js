4425a49e9d31b8492df70b853af963ad632e1cfd
orangex <test@qq.com>
2025-05-04 11:58:05 +0800

    home-goodsItem组件封装

--- src/apis/home.js
+++ src/apis/home.js
@@ -7,3 +7,36 @@ export function getBannerAPI(){
     url: '/home/banner'
   })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return httpInstance({
+    url:'home/hot'
+  })
+}
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
+  })
+}
--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import router from './router'
 // 引入初始化样式
 import '@/styles/common.scss'
 
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
+
